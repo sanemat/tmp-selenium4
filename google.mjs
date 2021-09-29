06fce15060814ac29a19d68f464b8453944b2c31
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-09-29 14:35:10 +0900

    chore(app): dockerize

--- google.mjs
+++ google.mjs
@@ -1,10 +1,16 @@
 import "chromedriver";
 import { Builder, By, until } from "selenium-webdriver";
+import { Options } from "selenium-webdriver/chrome.js";
+const options = new Options();
 
 let driver;
 
 try {
-  driver = await new Builder().forBrowser("chrome").build();
+  driver = await new Builder()
+    .forBrowser("chrome")
+    .usingServer("http://selenium-hub:4444/wd/hub")
+    .setChromeOptions(options)
+    .build();
   await driver.get("https://google.com/");
   await driver.wait(
     until.elementLocated(By.xpath("//img[contains(@alt,'Google')]")),
