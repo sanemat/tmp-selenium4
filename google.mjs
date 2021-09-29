192b69b60942beff7528b292aaec0724b3c95567
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-09-29 13:59:27 +0900

    chore(app): apply prettier

--- google.mjs
+++ google.mjs
@@ -4,9 +4,12 @@ import { Builder, By, until } from "selenium-webdriver";
 let driver;
 
 try {
-    driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://google.com/");
-    await driver.wait(until.elementLocated(By.xpath("//img[contains(@alt,'Google')]")), 5000);
+  driver = await new Builder().forBrowser("chrome").build();
+  await driver.get("https://google.com/");
+  await driver.wait(
+    until.elementLocated(By.xpath("//img[contains(@alt,'Google')]")),
+    5000
+  );
 } finally {
-    await driver.quit();
+  await driver.quit();
 }
