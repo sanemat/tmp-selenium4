fcafe9ecf05ee3c90128a09c0a2a52b51a031ba3
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-09-29 13:58:47 +0900

    chore(selenium): add small script

---
+++ google.mjs
@@ -0,0 +1,12 @@
+import "chromedriver";
+import { Builder, By, until } from "selenium-webdriver";
+
+let driver;
+
+try {
+    driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://google.com/");
+    await driver.wait(until.elementLocated(By.xpath("//img[contains(@alt,'Google')]")), 5000);
+} finally {
+    await driver.quit();
+}
